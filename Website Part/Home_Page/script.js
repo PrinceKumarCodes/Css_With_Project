9a64484177fee8f7eb87cb594d3d883b11484e21
PrinceKumarCodes <princekumarcbcs2022@gmail.com>
2025-03-16 13:45:19 +0530

    Update files

---
+++ Website Part/Home_Page/script.js
@@ -0,0 +1,6 @@
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("navMenu");
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
